875175f5bbe4e4a3e2c7792311632ef59d811f03
frazeedj <frazeedj@rose-hulman.edu>
2018-10-25 21:33:05 -0400

    ok

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Dylan Frazee.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -93,6 +93,16 @@ def problem2(sequence):
     # TODO: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    p = 0
+    for k in range(len(sequence)):
+        if sequence[k] < 0:
+            sequence[k] = sequence[k] * -1
+    for k in range(len(sequence)):
+        if sequence[k] == max(sequence):
+            p = k
+    return p
+
+
 
 
 ###############################################################################
